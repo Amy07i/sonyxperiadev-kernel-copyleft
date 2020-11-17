c33f76156d7712b4cbd070a0bcaefc8e92b6be27
Amy07i <371931720an@gmail.com>
2020-11-17 14:46:29 +0800

    build.sh: update
    Signed-off-by: Amy07i <371931720an@gmail.com>

--- build.sh
+++ build.sh
@@ -25,8 +25,10 @@ fi
 
 # package
 cp ${OUT_DIR}/kernel/out/arch/arm64/boot/Image.gz-dtb ${OUT_DIR}/AnyKernel3
+rm -rf ${OUT_DIR}/AnyKernel3/README.md LICENSE
 mkdir -p ${OUT_DIR}/AnyKernel3/modules/vendor/lib/modules/
 cp ${OUT_DIR}/kernel/out/drivers/staging/qcacld-3.0/wlan.ko ${OUT_DIR}/AnyKernel3/modules/vendor/lib/modules/qca_cld3_wlan.ko
 cd ${OUT_DIR}/AnyKernel3
 mkdir -p ${OUT_DIR}/upload
-zip -r ${OUT_DIR}/upload/StockPlusKernel-XZ1-Dual-${COMPILE_DATE}.zip *
+rm -rf ${OUT_DIR}/upload/*
+zip -r ${OUT_DIR}/upload/StockPlusKernel-XZ1-Dual-EAS-PELT-${COMPILE_DATE}.zip *
