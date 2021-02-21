ea43dd5c1a93bdaf52a93deca4ad034f516d4466
Amy07i <371931720an@gmail.com>
2021-02-21 23:36:49 +0800

    build.sh: add rebuilt modules
    Signed-off-by: Amy07i <371931720an@gmail.com>

--- build.sh
+++ build.sh
@@ -24,7 +24,13 @@ cd ${OUT_DIR}/AnyKernel3
 git reset --hard 0e26e385631a1afdfdf49d75b251d39a5a5d0b30
 cp ${OUT_DIR}/kernel/out/arch/arm64/boot/Image.gz-dtb ${OUT_DIR}/AnyKernel3
 mkdir -p ${OUT_DIR}/AnyKernel3/modules/vendor/lib/modules/
+cp ${OUT_DIR}/kernel/out/drivers/input/misc/bu520x1nvx.ko ${OUT_DIR}/AnyKernel3/modules/vendor/lib/modules/bu520x1nvx.ko
+cp ${OUT_DIR}/kernel/out/drivers/input/misc/fpc1145_platform.ko ${OUT_DIR}/AnyKernel3/modules/vendor/lib/modules/fpc1145_platform.ko
+cp ${OUT_DIR}/kernel/out/drivers/misc/ldo_vibrator.ko ${OUT_DIR}/AnyKernel3/modules/vendor/lib/modules/ldo_vibrator.ko
+cp ${OUT_DIR}/kernel/out/drivers/misc/pn553.ko ${OUT_DIR}/AnyKernel3/modules/vendor/lib/modules/nxp_pn553_nfc.ko
 cp ${OUT_DIR}/kernel/out/drivers/staging/qcacld-3.0/wlan.ko ${OUT_DIR}/AnyKernel3/modules/vendor/lib/modules/qca_cld3_wlan.ko
+cp ${OUT_DIR}/kernel/out/drivers/misc/sim_detect.ko ${OUT_DIR}/AnyKernel3/modules/vendor/lib/modules/sim_detect.ko
+
 mkdir -p ${OUT_DIR}/upload
 rm -rf ${OUT_DIR}/upload/*
 zip -r ${OUT_DIR}/upload/PureKernel-XZ1-Dual-${COMPILE_DATE}.zip *
